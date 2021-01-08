9de12e06221345ed49920fc755067b67a4fc35e9
vgerold- <eu@concreteresult.ru>
2021-01-08 20:00:26 +0100

    a

--- level8/source.c
+++ level8/source.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int	main()
 {
+	char*	service;
+	char*	auth;
+	void*	buf;
+	void*	edx;
+	int	ecx;
+	char*	str;
 
+	service = NULL;
+	auth = NULL;
+	buffer = NULL;
+	while (1)
+	{
+		printf("%p, %p\n", auth, service);
+		if (!(fgets((char*)buffer, 0x80, stdin)))
+			break;
+		if(!(strncmp("auth ", (char*)buffer, 0x5)))
+		{
+			str = malloc(4)
+		}
+
+	}
+	return (0);
 }
