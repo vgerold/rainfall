22e2ea245ec64d9657820cf68c2e74ff5ad5ee18
vgerold <eu@concreteresult.ru>
2021-01-10 04:23:27 +0300

    lvl8 walktrhough

--- level8/source.c
+++ level8/source.c
@@ -6,10 +6,7 @@ int	main()
 {
 	char*	service;
 	char*	auth;
-	void*	buf;
-	void*	edx;
-	int	ecx;
-	char*	str;
+	char	buffer[128];
 
 	service = NULL;
 	auth = NULL;
@@ -17,13 +14,27 @@ int	main()
 	while (1)
 	{
 		printf("%p, %p\n", auth, service);
-		if (!(fgets((char*)buffer, 0x80, stdin)))
+		if (!(fgets(buffer, 64, stdin)))
 			break;
-		if(!(strncmp("auth ", (char*)buffer, 0x5)))
+		if (!(strncmp("auth ", buffer, 5)))
 		{
-			str = malloc(4)
+			auth = malloc(4);
+			bzero((void*)auth, 4);
+			(void*)buffer = (long)0;
+			if (strlen(buffer + 5) - 1 < 30)
+				strcpy(auth, buffer + 5);
 		}
-
+		if (!(strncmp("reset", buffer, 5)))
+			free(auth);
+		if (!(strncmp("service", buffer, 6)))
+			service = strdup(buffer + 7);
+		if (!(strncmp("login", buffer, 5)))
+		{
+			if (auth[32])
+				system("/bin/sh");
+			else
+				fwrite(&"Password:\n", 10, 1, stdout);
+		}
 	}
 	return (0);
 }
