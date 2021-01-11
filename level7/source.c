cf8eae617f5daaeedeac5e4bfd242f1f882a5a37
ergottli <ergottli@student.21-school.ru>
2021-01-12 02:40:33 +0300

    fix sources 7

--- level7/source.c
+++ level7/source.c
@@ -1,37 +1,39 @@
 # include <stdlib.h>
 # include <string.h>
 # include <stdio.h>
-# include < time.h >
+# include <time.h>
 
-int c = 0;
+char	c[68];
+
+typedef struct t {
+    int num;
+    char *data;
+} st;
+
 
 int m()
 {
 	int t;
 	t = time(NULL);
-	return printf("%s - %d\n", (char*)&c, t);
+	return printf("%s - %d\n", c, t);
 }
 
 int main(int argc, char **argv)
 {
-	int *ptr;
-	int *ptr2;
-	int *ptr3;
-	int *ptr4;
+    st *ptr1;
+    st *ptr2;
 
-	ptr = malloc(8);
-	*ptr = 1;
-	ptr2 = malloc(8);
-	ptr[1] = (int)ptr2;
-	ptr3 = malloc(8);
-	*ptr3 = 2;
-	ptr4 = malloc(8);
-	ptr3[1] = (int)ptr4;
+    ptr1 = malloc(8);
+    ptr1->num = 1;
+    ptr1->data = (char*)malloc(8);
+    ptr2 = malloc(8);
+    ptr2->num = 2;
+    ptr2->data = (char*)malloc(8);
 
-	strcpy((char*)ptr[1], argv[1]);
-	strcpy((char*)ptr3[1], argv[2]);
+	strcpy(ptr1->data, argv[1]);
+	strcpy(ptr2->data, argv[2]);
 	FILE *file = fopen("/home/user/level8/.pass", "r");
-	fgets((char*)&c, 68, file);
+	fgets(c, 68, file);
 	puts("~~");
 	return (0);
 }
