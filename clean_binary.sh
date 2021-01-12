3094733547bc4c0670000cc7a8a580fa381ac026
vgerold <eu@concreteresult.ru>
2021-01-13 02:44:11 +0300

    a

--- clean_binary.sh
+++ clean_binary.sh
@@ -3,4 +3,4 @@
 echo "Clearing"
 dir=level; i=0; while [ $i -lt 9 ]; do rm ./$dir$i/$dir$i 2>/dev/null && echo "$dir$i binary deleted"; (( i++ )); done
 dir=bonus; i=0; while [ $i -lt 4 ]; do rm ./$dir$i/$dir$i 2>/dev/null && echo "$dir$i binary deleted"; (( i++ )); done
-
+rm ./level9/level9
