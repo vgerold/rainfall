6952e83f6dc36c2d132ff57f620ef32367b51ced
vgerold <eu@concreteresult.ru>
2021-01-13 02:42:54 +0300

    make_binary

--- make_binary.sh
+++ make_binary.sh
@@ -3,15 +3,18 @@
 echo "Clearing"
 dir=level; i=0; while [ $i -lt 9 ]; do rm ./$dir$i/$dir$i 2>/dev/null && echo "$dir$i binary deleted"; (( i++ )); done
 dir=bonus; i=0; while [ $i -lt 4 ]; do rm ./$dir$i/$dir$i 2>/dev/null && echo "$dir$i binary deleted"; (( i++ )); done
+rm ./level9/level9 2>/dev/null; 
 
 echo "Compiling"
 dir=level; i=0; while [ $i -lt 9 ]; do cp ./$dir$i/source* ./$dir$i/source.c; (( i++ )); done
 dir=bonus; i=0; while [ $i -lt 4 ]; do cp ./$dir$i/source* ./$dir$i/source.c; (( i++ )); done
+cp ./level9/source ./level9/source.c && g++ ./level9/source.c -o ./level9/level9 ; 
 dir=level; i=0; while [ $i -lt 9 ]; do gcc -fno-stack-protector -z execstack -m32 -g ./$dir$i/source.c -o ./$dir$i/$dir$i; (( i++ )); done
 dir=bonus; i=0; while [ $i -lt 4 ]; do gcc -fno-stack-protector -z execstack -m32 -g ./$dir$i/source.c -o ./$dir$i/$dir$i; (( i++ )); done
 
 echo "Clearing"
 dir=level; i=0; while [ $i -lt 9 ]; do rm ./$dir$i/source.c 2>/dev/null && echo "$dir$i/source.c deleted "; (( i++ )); done
 dir=bonus; i=0; while [ $i -lt 4 ]; do rm ./$dir$i/source.c 2>/dev/null && echo "$dir$i/source.c deleted"; (( i++ )); done
+rm ./level9/source.c 2>/dev/null; 
 
 
